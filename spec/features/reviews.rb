303ee62ec10a737d6b1964bb303bf5fa020350cd
Khurram Virani <kvirani@gmail.com>
2013-11-28 19:30:25 +0000

    make the delete review feature work (completed)

--- spec/features/reviews.rb
+++ spec/features/reviews.rb
@@ -2,14 +2,41 @@ require 'spec_helper'
 
 feature "Review Delete link" do 
 
-  scenario "should be displayed to the user that wrote the review" do 
-    create_user_and_login
-    @review = FactoryGirl.create :review, user: @user
-    @movie = @review.movie
+  context "while logged in" do 
+    before :each do 
+      create_user_and_login
+    end
+    
+    scenario "should be displayed to the user that wrote the review" do   
+      @review = FactoryGirl.create :review, user: @user
+      @movie = @review.movie
 
-    visit movie_path(@movie) # visit "/movies/#{@movie.id}"
+      visit movie_path(@movie) # visit "/movies/#{@movie.id}"
+      expect(page).to have_link 'Delete Review'
+    end
 
-    expect(page).to have_link 'Delete Review'
+    scenario "should not be displayed to the user that didn't write the review" do 
+      @review = FactoryGirl.create :review
+      @movie = @review.movie
+
+      visit movie_path(@movie) # visit "/movies/#{@movie.id}"
+      expect(page).to_not have_link 'Delete Review'
+    end
+
+    scenario "should delete review when clicked" do 
+      @review = FactoryGirl.create :review, user: @user
+      @movie = @review.movie      
+
+      visit movie_path(@movie)
+      click_link 'Delete Review'
+      expect(page).to have_content("Deleted your review")
+      expect(page).to_not have_content(@review.text)
+    end
+  end
+
+  context "while not logged in" do
+    pending "should not be displayed to the user even if they own the review"
+    pending "shows message to user that they should login to review a movie"
   end
 
 end
